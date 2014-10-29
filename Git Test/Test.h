0a6141fef680fb7c73e578ca287d113929f958af
Hien Le <hienle@Hiens-Mac-Pro.local>
2014-10-29 14:11:59 -0500

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Hien Le on 10/29/14.
+//  Copyright (c) 2014 Hien Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Hien Le on 10/29/14.
+//  Copyright (c) 2014 Hien Le. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
